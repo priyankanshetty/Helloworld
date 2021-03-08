9772d67a86b0f90a76deb745e57a02de5936c5b0
Priyanka Shetty <priyanka.shetty@maistering.com>
2021-03-08 15:58:38 +0530

    main

---
+++ HelloWorld/Service/HelloWorldService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Service
+{
+    public class HelloWorldService
+    {
+        public string GetHelloWorld()
+        {
+            return "HelloWorld";
+        }
+    }
+}
